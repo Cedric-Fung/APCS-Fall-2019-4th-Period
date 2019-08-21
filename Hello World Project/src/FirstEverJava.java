b8224561a7eb7c63729e7b7276df1b917872f297
APCS2nd <APCS2nd@MHS-LT9-35>
2019-08-21 12:19:30 -0700

    Complete Hello World Project

---
+++ Hello World Project/src/FirstEverJava.java
@@ -0,0 +1,16 @@
+//Cedric Fung
+//August 16, 2019
+//First class made in Java for APCS
+/*Prints a message to the screen*/
+public class FirstEverJava {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Hello cedric");
+		System.out.print("First java,"); 
+		System.out.print(" This class seems hard,");
+		System.out.println(" APCS");
+		System.out.println("Hello Mills");
+	}
+
+}
