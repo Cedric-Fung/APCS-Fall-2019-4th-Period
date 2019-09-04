06db01917d66149d2a9b7d638d49690f97969f75
APCS2nd <APCS2nd@MHS-LT9-35>
2019-09-04 12:03:04 -0700

    Commit through average

---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,28 @@
+/*Cedric Fung
+*August 29, 2019
+*Period 4
+*Group of methods that perform math calculations (a library)
+*/
+public class Calculate { 
+	//A call to square returns the square of the value passed
+	//it accepts and returns an integer
+	public static int square(int number) {
+		int answer=0;
+		answer = number*number;
+		return answer;
+	}
+	//A call to cube returns the cube of the value passed
+	//it accepts and returns an integer
+		public static int cube(int number) {
+			int answer=0;
+			answer = number*number*number;
+			return answer;
+		}
+		//A call to average returns the average of the value passed to it
+		//This method accepts two doubles and returns a double
+			public static int average(int number) {
+				int answer=0;
+				answer = number*number*number;
+				return answer;
+		
+}
