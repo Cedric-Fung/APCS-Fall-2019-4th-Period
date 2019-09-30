a81eb40d20c326f6f0cc194a48324fbd7689178f
Cedric-Fung <54287760+Cedric-Fung@users.noreply.github.com>
2019-09-29 20:12:57 -0700

    Update QuadraticClient.java

--- QuadraticDescriber/src/QuadraticClient.java
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -1,9 +1,28 @@
+//Cedric Fung
+//4th Period 
+//September 29 2019
+//This code takes inputs and outputs and calls quadrtic describer
 
+import java.util.*;
 public class QuadraticClient {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public static void main(String[] args) {
+System.out.println("This is the Quadratic Describer");
+System.out.println("This provides the values for variables a, b, and c");
+boolean continues = true;
+Scanner scanner = new Scanner(System.in);
+while(continues)
+System.out.println(Quadratic.quadrDescriber(0,0,0));
+System.out.println("Do you want to do more? (Typer \"quit\" to end)");
+String answer = scanner.nextLine();
+	answer = answer.toUppeCase();
+if (answer.equals("quit")) {
+	continues = false;
+}
+else{ 
+	continues = true;
+}
+}
+	scanner.close();
 	}
 
 }
